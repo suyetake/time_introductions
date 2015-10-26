e879b25d7ef0785a89461ca65341bfe19ceea1bd
Sue Uyetake <sue.uyetake@gmail.com>
2015-10-25 19:12:26 -0600

    refactored to final solution
    eliminated hello views and welcome controller
    placed string conversion actions in introductions_controller
    (instead of converting parameters in view)
    kept Time.new actions in time_controller, not in views
    
    NOTE: Rubocop seems happy, yet Guard and rspec are not happy
    This is acknowledged and okay for this stage of the rails app.
    
    [#106275258]

--- app/controllers/welcome_controller.rb
+++
@@ -1,8 +0,0 @@
-class WelcomeController < ApplicationController
-  def hello
-  end
-  def say_hello_to_someone
-  end
-  def say_hello_to_two_people
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'hello' => 'welcome#hello'
-  get 'hello/:name' => 'welcome#say_hello_to_someone'
-  get 'hello/:name1/:name2' => 'welcome#say_hello_to_two_people'
   get 'time/now' => 'time#display_current_time'
   get ':name1/and/:name2' => 'introductions#introduce_two_people'
 end
