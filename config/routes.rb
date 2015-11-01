3a2cdbc2973db287de1c7b3ee2c330374675da11
Sue Uyetake <sue.uyetake@gmail.com>
2015-11-01 01:12:10 -0600

    corrected outputs and refactored after story rejection
    introductions corrected in routes.rb
    time display corrected in html, and moved conversions into controller
    
    [#106275258]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'time/now' => 'time#display_current_time'
-  get ':name1/and/:name2' => 'introductions#introduce_two_people'
+  get '/introduce/:name1/and/:name2' => 'introductions#introduce_two_people'
 end
 
